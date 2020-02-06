645ab19ca0f25023ebe7a325003c7023957ad113
Rutuja Rajesh <rutujarajesh23@gmail.com>
2020-02-06 15:27:48 +0530

    Create record_and_playback_normal.vb

---
+++ 1/record_and_playback_normal.vb
@@ -0,0 +1,5 @@
+WpfWindow("HPE MyFlight Sample Applicatio").Click 344,88
+WpfWindow("HPE MyFlight Sample Applicatio").WpfEdit("agentName").Set "john"
+WpfWindow("HPE MyFlight Sample Applicatio").WpfEdit("password").SetSecure "5d7742cda10cb57ccbdf"
+WpfWindow("HPE MyFlight Sample Applicatio").WpfButton("OK").Click
+WpfWindow("HPE MyFlight Sample Applicatio").Close
