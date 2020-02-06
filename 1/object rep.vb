0797c2ba781a0b864b66e047a96c0e97a6e254c0
Rutuja Rajesh <rutujarajesh23@gmail.com>
2020-02-06 15:25:40 +0530

    Create object rep.vb

---
+++ 1/object rep.vb
@@ -0,0 +1,5 @@
+WpfWindow("HPE MyFlight Sample Applicatio").WpfEdit("agentName").Set "john"
+WpfWindow("HPE MyFlight Sample Applicatio").WpfEdit("password").SetSecure "5d7769a99e22c261b514"
+WpfWindow("HPE MyFlight Sample Applicatio").WpfButton("login").Click
+WpfWindow("HPE MyFlight Sample Applicatio").Move 235,22
+WpfWindow("HPE MyFlight Sample Applicatio").Close
