0493efa23387f081785ffb6339e72b37a7bc0253
Rutuja Rajesh <rutujarajesh23@gmail.com>
2020-02-06 15:26:56 +0530

    Create record_and_playback_low_level.vb

---
+++ 1/record_and_playback_low_level.vb
@@ -0,0 +1,6 @@
+Window("HPE MyFlight Sample Applicatio").Click 256,214
+Window("HPE MyFlight Sample Applicatio").Type "john"
+Window("HPE MyFlight Sample Applicatio").Click 182,282
+Window("HPE MyFlight Sample Applicatio").Type "hp"
+Window("HPE MyFlight Sample Applicatio").Click 145,356
+Window("HPE MyFlight Sample Applicatio_2").Close
