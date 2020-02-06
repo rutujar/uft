8636d8e62b7d835244f4dcbd8f94b4297e7e8c33
Rutuja Rajesh <rutujarajesh23@gmail.com>
2020-02-06 15:30:01 +0530

    Create login.vb

---
+++ 2/data driven testing actionsheet/login.vb
@@ -0,0 +1,5 @@
+SystemUtil.Run "C:\Program Files (x86)\HPE\Unified Functional Testing\samples\Flights Application\FlightsGUI.exe"
+
+WpfWindow("HPE MyFlight Sample Applicatio").WpfEdit("agentName").Set "john"
+WpfWindow("HPE MyFlight Sample Applicatio").WpfEdit("password").SetSecure "5d78b763f74542c608b5"
+WpfWindow("HPE MyFlight Sample Applicatio").WpfButton("OK").Click
