16fbfdf2549213cd92db1040ddfa36c24c8282e3
Rutuja Rajesh <rutujarajesh23@gmail.com>
2020-02-06 15:26:29 +0530

    Create record_and_playback_insight.vb

---
+++ 1/record_and_playback_insight.vb
@@ -0,0 +1,3 @@
+WpfWindow("HPE MyFlight Sample Applicatio").InsightObject("InsightObject").Click
+WpfWindow("HPE MyFlight Sample Applicatio").InsightObject("InsightObject_2").Click
+WpfWindow("HPE MyFlight Sample Applicatio").InsightObject("InsightObject_3").Click
